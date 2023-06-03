5a458f3d1b7320c9ce3a79fd50271baf150fdf07
amirrezaask <raskarpour@gmail.com>
2023-06-03 10:42:11 +0330

    - Last Two elements of the list - nth of list

--- src/lists.ml
+++ src/lists.ml
@@ -1,6 +1,28 @@
-let rec tail_of_a_list = function
+let rec last = function
     | [] -> None
     | h :: [] -> Some h
-    | _ :: tl -> tail_of_a_list tl
+    | _ :: tl -> last tl
 ;;
+
+
+let rec last_two = function
+    | [] -> None 
+    | a :: [] -> None
+    | a :: b :: [] -> Some (a, b)
+    | _ :: tl -> last_two tl
+;;
+
+
+let rec nth l n =
+    match l with
+    | [] -> None
+    | a :: tl -> match n with
+        | 0 -> Some a
+        | _ -> nth tl (n - 1)
+;;
+
+
+
+    
+
 
