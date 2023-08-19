a49a8c8be05f0902cb035beac390df8d8e68bfb5
amirrezaask <raskarpour@gmail.com>
2023-08-19 23:44:29 +0330

    Run-Length Encoding

--- problems.ml
+++ problems.ml
@@ -62,12 +62,11 @@ let compress l =
 let pack l =
   let rec aux l buffer out =
     match l with
-    | [] -> if (length buffer) = 0 then out else buffer :: out
-    | x :: t -> (
-      match buffer with
-      | [] -> aux t [x] out
-      | y :: _ -> (if x = y then aux t (x::buffer) out else aux t [x] (buffer :: out))
-    )
+    | [] -> if length buffer = 0 then out else buffer :: out
+    | x :: t ->
+      (match buffer with
+       | [] -> aux t [ x ] out
+       | y :: _ -> if x = y then aux t (x :: buffer) out else aux t [ x ] (buffer :: out))
   in
   aux l [] [] |> reverse
 ;;
@@ -77,15 +76,14 @@ let pack2 l =
   let rec aux l out =
     match l with
     | [] -> out
-    | x :: t -> (
-      match out with
-      | [] -> aux t ([x] :: out)
-      | buffer :: tl ->
-        match buffer with
-        | [] -> [x] :: out
-        | y :: buffertl when x = y -> aux t ((x :: y :: buffertl) :: tl)
-        | _ -> aux t ([x] :: out)
-    )
+    | x :: t ->
+      (match out with
+       | [] -> aux t ([ x ] :: out)
+       | buffer :: tl ->
+         (match buffer with
+          | [] -> [ x ] :: out
+          | y :: buffertl when x = y -> aux t ((x :: y :: buffertl) :: tl)
+          | _ -> aux t ([ x ] :: out)))
   in
   aux l [] |> reverse
 ;;
@@ -104,6 +102,25 @@ let print_list l =
   printf "]"
 ;;
 
+let print_tuple (a, b) =
+  Stdio.printf "%d, %s\n" a b
+
+let encode l =
+  let rec aux list last counter out =
+    match list with
+      | [] -> (match last with
+        | Some c -> (counter, c) :: out
+        | None -> out)
+      | h :: tl ->
+        match last with
+        | None -> aux tl (Some h) 1 out
+        | Some c when c = h -> aux tl (Some h) (counter+1) out
+        | Some c -> aux tl (Some h) 1 ((counter, c) :: out)
+
+  in
+
+  aux l None 0 [] |> reverse
+
 let () =
   let _ = last [ "a"; "b"; "c"; "d" ] in
   let _ = last_two [ "a"; "b"; "c"; "d" ] in
@@ -111,49 +128,10 @@ let () =
   let _ = length [ "a"; "b"; "c" ] in
   let _ = reverse [ "a"; "b"; "c" ] in
   let _ = is_palindrome [ "x"; "a"; "m"; "a"; "x" ] in
-  let _ =
-    flatten [ One "a"; Many [ One "b"; Many [ One "c"; One "d" ]; One "e" ] ]
-  in
-  let _ =
-    compress
-      [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e" ]
-  in
-  let _ =
-    pack
-      [ "a"
-      ; "a"
-      ; "a"
-      ; "a"
-      ; "b"
-      ; "c"
-      ; "c"
-      ; "a"
-      ; "a"
-      ; "d"
-      ; "d"
-      ; "e"
-      ; "e"
-      ; "e"
-      ; "e"
-      ]
-  in
-  let p2 = pack2
-  [ "a"
-  ; "a"
-  ; "a"
-  ; "a"
-  ; "b"
-  ; "c"
-  ; "c"
-  ; "a"
-  ; "a"
-  ; "d"
-  ; "d"
-  ; "e"
-  ; "e"
-  ; "e"
-  ; "e"
-  ] in
-  let _ = List.map print_list p2 in
+  let _ = flatten [ One "a"; Many [ One "b"; Many [ One "c"; One "d" ]; One "e" ] ] in
+  let _ = compress [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e" ] in
+  let _ = pack [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "d"; "e"; "e"; "e"; "e" ] in
+  let _ = pack2 [ "a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "d"; "e"; "e"; "e"; "e" ] in
+  let _ = encode ["a"; "a"; "a"; "a"; "b"; "c"; "c"; "a"; "a"; "d"; "e"; "e"; "e"; "e"] |> List.map print_tuple in
   ()
 ;;
